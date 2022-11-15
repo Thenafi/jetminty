5e839be33180f3ce7c25ad796d1ac604d795bf04
Thenafi <65831153+Thenafi@users.noreply.github.com>
2022-11-15 16:41:16 +0600

    test

--- src/routes/pdfapi/pdf.ts
+++ src/routes/pdfapi/pdf.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+const dotenv = require("dotenv");
+dotenv.config();
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-    res.send('At PDF route')
+    res.send({
+        type: process.env.TYPE,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        refresh_token: process.env.REFRESH_TOKEN,
+    })
 }
 )
 
